a77b85151ab6be0eb568cad677d334738b22d652
Suhasp987 <ps9781094@gmail.com>
2024-07-18 11:23:03 +0530

    socket

---
+++ index.js
@@ -0,0 +1,29 @@
+const express=require("express");
+const app=express();
+const cors=require("cors")
+const http=require("http");
+const {Server} = require("socket.io")
+
+app.use(cors());
+const server=http.createServer(app);
+
+const io=new Server(server,{
+    cors:{
+        origin:"http://localhost:3000",
+      
+    }
+})
+
+
+io.on('connection',(socket)=>{
+
+    
+      socket.on("message",(data)=>{
+           socket.broadcast.emit("recieved",data)
+      })
+})
+
+server.listen(3001,()=>{
+    console.log("server is running")
+})
+
