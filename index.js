d4c7e897b21a36e17422788e58a03f28e07a2f32
Suhasp987 <ps9781094@gmail.com>
2024-07-18 12:08:13 +0530

    po

--- index.js
+++ index.js
@@ -7,29 +7,29 @@ const { Server } = require("socket.io");
 app.use(cors());
 const server = http.createServer(app);
 
-const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:3000", // Update this based on your frontend URL
-    methods: ["GET", "POST"],
-  },
-});
+// const io = new Server(server, {
+//   cors: {
+//     origin: "http://localhost:3000", // Update this based on your frontend URL
+//     methods: ["GET", "POST"],
+//   },
+// });
 
 app.get("/test", (req, res) => {
   res.json({ message: "working" });
 });
 
-io.on("connection", (socket) => {
-  console.log("a user connected");
+// io.on("connection", (socket) => {
+//   console.log("a user connected");
 
-  socket.on("message", (data) => {
-    console.log("message received:", data);
-    socket.broadcast.emit("received", data);
-  });
+//   socket.on("message", (data) => {
+//     console.log("message received:", data);
+//     socket.broadcast.emit("received", data);
+//   });
 
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
-  });
-});
+//   socket.on("disconnect", () => {
+//     console.log("user disconnected");
+//   });
+// });
 
 const PORT = process.env.PORT || 3001;
 
